56dc6186e0512092e0b4f8dfd5bcf2ae3a2ee411
Joao-Moura <jaummoura15@gmail.com>
2023-12-03 16:53:17 -0300

    Adicionando executor de benchmark

---
+++ benchmark/benchmark.py
@@ -0,0 +1,54 @@
+import os
+import time
+import sys
+
+import subprocess
+
+from glob import glob
+
+from validador import valida_jogadas
+
+
+def remove_extra_files(files):
+    for arquivos_extras in files:
+        try:
+            os.remove(arquivos_extras)
+        except FileNotFoundError:
+            continue
+
+
+def main():
+    subprocess.run(['./build.sh'] + (sys.argv[1:] if len(sys.argv) > 2 else []))
+    mapas = glob('benchmark/mapas/*')
+    mapas.sort()
+
+    tempo_total_final = 0
+
+    for mapa in mapas:
+        remove_extra_files(['problema.hddl', 'domain.hddl'])
+        f = open(mapa, 'r')
+        entrada = f.read()
+        f.seek(0)
+
+        initial_time = time.time()
+        retorno = subprocess.run(
+            ['python3', 'wfc.py3'], stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        tempo_total = time.time() - initial_time
+
+        f.close()
+
+        retorno_stdout = retorno.stdout.decode('utf-8')
+
+        tempo_total_final += tempo_total
+        qtd_passos = len(retorno_stdout.split('\n'))
+        vitoria = 'Sucesso' if valida_jogadas(entrada, retorno_stdout[:-1]) else 'Derrota'
+
+        retorno_falha = f'\n|_ Plano encontrado: [{retorno_stdout[:-1]}]' if vitoria == 'Derrota' else ''
+        print(f'Executado mapa {mapa} com {qtd_passos} passos em {tempo_total}s [{vitoria}]' + retorno_falha)
+
+        remove_extra_files(glob('problem--domain*'))
+
+    print(f'Tempo total final: {tempo_total_final}s')
+
+if __name__ == "__main__":
+    main()
